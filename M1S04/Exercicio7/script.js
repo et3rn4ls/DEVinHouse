ae04eb9fd3f8e10ef83563daa34a36d0202b3442
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-03-24 11:02:09 -0300

    M1S04 - exercicio 7

---
+++ M1S04/Exercicio7/script.js
@@ -0,0 +1,9 @@
+// M1S04 - Exercicio 7
+
+class Conta {
+    constructor(conta, saldo, cliente) {
+        this.conta = conta;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+}
