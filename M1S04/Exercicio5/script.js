007b29b8c082fbea195b33887034a0dc9f4f9743
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-03-24 10:47:15 -0300

    M1S04 - exercicio 5

---
+++ M1S04/Exercicio5/script.js
@@ -0,0 +1,12 @@
+// M1S04 - Exercicio 5
+
+class Endereco {
+    constructor(logradouro, numero, cidade, estado, pais, cep) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+    }
+}
