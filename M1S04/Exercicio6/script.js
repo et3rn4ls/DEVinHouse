7954ce902535f19fec876bceabccbe1cd22b0645
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-03-24 10:58:35 -0300

    M1S04 - exercicio 6

---
+++ M1S04/Exercicio6/script.js
@@ -0,0 +1,10 @@
+// M1S04 - Exercicio 6
+
+class Cliente {
+    constructor(nome, cpf, endereco, celular) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.celular = celular;
+    }
+}
