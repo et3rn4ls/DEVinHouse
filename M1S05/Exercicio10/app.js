63b1c59594fbdbed769e332804ea19758db3dd2e
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-03-31 10:36:39 -0300

    M1S05 - exercicio 10

---
+++ M1S05/Exercicio10/app.js
@@ -0,0 +1,15 @@
+document.getElementById("cat").addEventListener("click", novaImagem);
+
+async function novaImagem(){    
+    try {
+        await fetch('https://api.thecatapi.com/v1/images/search')
+            .then(response => response.json())
+            .then((data) => {
+                let img = data[0].url
+                document.getElementById("img").src=`${img}`;
+            })
+    }
+    catch (err) {
+          document.getElementById("retorno").innerHTML = `Ocorreu um erro: ${err}!`
+    }
+}
