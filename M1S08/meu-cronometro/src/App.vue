eec1e83bd0619edbce4f0673da69612875943c10
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-27 16:58:37 -0300

    M1S08 - Exercicio1

---
+++ M1S08/meu-cronometro/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <CronCrontrol />
+  </div>
+</template>
+
+<script>
+import CronCrontrol from './components/CronControl.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CronCrontrol
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ M1S08/meu-cronometro/src/components/CronControl.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="cronometro">
+      <button class="btn btn-outline-dark btn-sm" @click="play">{{timer !== null ? "PAUSAR" :"VAI" }}</button>
+      <button class="btn btn-outline-dark btn-sm" @click="clear">LIMPAR</button>
+      <br><br>
+      <a class="timer">{{zfill(hour)}}:{{zfill(min)}}:{{zfill(sec)}}</a>
+      <div class="interval" v-show="intervalList.length > 0">
+      <br><br>
+      <ul>
+        <li v-for="interval in intervalList" :key="interval" >VOCÊ PAUSOU EM {{interval}}</li>
+      </ul>
+      <button class="btn btn-outline-dark btn-sm" @click="clearIntervalList">LIMPAR HISTÓRICO</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      sec: 0,
+      min: 0,
+      hour: 0,
+      timer: null,
+      intervalList: []
+    }
+  },
+  methods: {
+    zfill(number){
+      return number.toString().padStart(2,0)
+    },
+    play(){      
+      if(this.timer === null){
+        this.playing()
+        this.timer = setInterval(()=> this.playing(), 1000)
+      }
+      else{
+        clearInterval(this.timer);
+        this.timer = null;
+        this.pause();
+      }
+    },
+    playing(){
+      this.sec++
+      if(this.sec >= 59){
+        this.sec = 0;
+        this.min++;
+      }
+      if(this.min >= 59){
+        this.min = 0;
+        this.hour++;
+      }
+    },
+    pause(){
+      this.intervalList.push(`${this.zfill(this.hour)}:${this.zfill(this.min)}:${this.zfill(this.sec)}`)
+      console.log(this.intervalList)
+    },
+    clear(){
+      if(this.timer !== null){
+        clearInterval(this.timer)
+        this.timer = null
+      }
+      this.sec = 0;
+      this.min = 0;
+      this.hour = 0;
+      this.clearIntervalList();
+    },
+    clearIntervalList(){
+      this.intervalList = []
+      console.log(this.intervalList)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
