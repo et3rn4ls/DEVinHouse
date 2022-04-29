ce2f0d5412c99def7236c0f0ef4c24836dd3e134
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-29 13:51:50 -0300

    M1S09 - Exercicio7

---
+++ M1S09/exercicio-07/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+      <my-header :title="title" v-show="title"></my-header>
+      <hr>
+      <input type="text" v-model="title">
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'my-header': MyHeader
+  },
+  data() {
+    return {
+      title: 'M1S09 - Exercício 07'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ M1S09/exercicio-07/src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+      <h1 v-text="title"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  props: ['title']
+}
+</script>
+
+<style>
+</style>
