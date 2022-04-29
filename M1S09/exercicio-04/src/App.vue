9a46b2eec3ffedc78c311b1829f013c655e3a3fc
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-29 11:31:12 -0300

    M1S09 - Exercicio 4

---
+++ M1S09/exercicio-04/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app">
+        <h3 v-text="msg"></h3>
+        <hr>
+        <label :style="`color:${corTexto}`">Cor</label>
+        <br>
+        <input type="text" v-model="corTexto">
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'M1S09 - Exercício 4',
+      corTexto: null
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+h3 {
+    color:slategray;
+}
+</style>
