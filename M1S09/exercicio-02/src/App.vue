c086d739bd6468f3244592c755557cb9b1371660
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-29 08:14:01 -0300

    M1S09 - Exercicio2

---
+++ M1S09/exercicio-02/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app">
+        <h1 v-once v-text="msg"></h1>
+        <input type="text" v-model="msg">
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'M1S09 - Exercício 2'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
