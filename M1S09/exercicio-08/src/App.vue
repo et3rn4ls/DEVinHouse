1d0ad8465fec4ee96bcfafe5de33c358d605a621
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-29 15:01:07 -0300

    M1S09 - Exercicio9

---
+++ M1S09/exercicio-08/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <h3>Formulário:</h3>
+    <form @submit.prevent="salvar">
+      <div class="row g-2">
+        <div class="col-md-2">
+          <label for="">Nome</label>
+          <input type="text" v-model="pessoa.nome">
+        </div>
+        <div class="col-md-2">
+          <label for="">Idade</label>
+          <input type="number" v-model="pessoa.idade">
+        </div>
+        <div class="col-sm-12">
+          <button type="submit" class="btn btn-primary btn-sm">Adicionar</button>
+        </div>
+      </div>
+    </form>
+    <hr>
+    <h3>Lista:</h3>
+    <div v-if="pessoas.length === 0">
+      Não há informações cadastradas
+    </div>
+    <table class="table" v-else>
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Idade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(p, i) in pessoas" :key="i">
+            <td v-text="p.nome"></td>
+            <td v-text="p.idade"></td>
+          </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      pessoas: [],
+      pessoa: {}
+    }
+  },
+  methods: {
+    salvar() {
+      this.pessoa.id = this.pessoas.length + 1;
+      this.pessoas.push(this.pessoa);
+      this.pessoa = {};
+    }
+  }
+}
+</script>
+
+<style>
+</style>
