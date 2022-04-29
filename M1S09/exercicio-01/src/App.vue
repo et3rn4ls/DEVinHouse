49889de94c424c59b23cb20660aee56a3025b1b2
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-04-29 08:01:36 -0300

    M1S09 - Exercicio1

---
+++ M1S09/exercicio-01/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="exercicio">
+        <h1 v-text="msg"></h1>
+        <input type="text" v-model="msg">
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'M1S09 - Exercício 1'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
