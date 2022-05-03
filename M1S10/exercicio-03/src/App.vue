6ba086259477afdbe10b4bc5107c0d02d5c2938f
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-05-03 11:16:13 -0300

    M1S10 - Exercicio 03

---
+++ M1S10/exercicio-03/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app">
+    <h2>M1S10 - Exercício 03</h2>
+    <hr>
+    <div>
+      <button @click="show = !show">Alterar estado</button>
+      <br><br>
+      <transition name="header" mode="out-in">
+        <header v-if="show">Apresentando o H1</header>
+        <span v-else>O header não está disponível</span>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.header-leave-to,
+.header-enter-from {
+  opacity: 0;
+}
+
+.header-leave-active,
+.header-enter-active {
+  transition: opacity 2s;
+}
+
+.header-leave-from,
+.header-enter-to {
+  opacity: 1;
+}
+</style>
