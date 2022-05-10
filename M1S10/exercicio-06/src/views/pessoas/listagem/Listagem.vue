d7e799ddb21e1cb1d7f57622ed02fdf15283c9e8
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-05-10 18:51:06 -0300

    M1S10 - Exercicio 06

---
+++ M1S10/exercicio-06/src/views/pessoas/listagem/Listagem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="listagem">
+      <h3>Listagem</h3>
+      <br><br>
+      <div>
+        <button type="button" class="btn btn-outline-primary btn-sm" @click="buscarPessoas">Listar Pessoas</button>
+      </div>
+      <div>{{ listaPessoas }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listaPessoas: []
+    }
+  },
+  methods: {
+    buscarPessoas() {
+      this.listaPessoas = this.$store.state.pessoas;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
