54aeb19bcb2b6648d237541690e84749fd58801c
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-05-10 19:03:05 -0300

    M1S10 - Exercicio 07

---
+++ M1S10/exercicio-07/src/views/pessoas/cadastro/Cadastro.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="cadastro">
+      <h3>Cadastro</h3>
+      <br><br>
+      <vee-form @submit="salvarPessoa" :validation-schema="schema" v-slot="{ errors }">
+        <div class="row g-2">
+          <div class="col-md-4">
+            <label for="nome">Nome Completo: </label>
+            <vee-field name="nome" type="text" v-model="pessoa.nome"/>
+            <span class="text-danger" v-text="errors.nome" v-show="errors.nome"></span>
+          </div>
+          <div class="col-3">
+            <label for="dataNascimento">Data de nascimento: </label>
+            <vee-field name="dataNascimento" type="date" v-model="pessoa.dataNascimento"/>
+            <span class="text-danger" v-text="errors.dataNascimento" v-show="errors.dataNascimento"></span>
+          </div>
+          <div class="col-3">
+            <button type="submit" class="btn btn-outline-primary btn-sm">Cadastrar</button>
+          </div>
+        </div>
+      </vee-form>
+  </div>
+</template>
+
+<script>
+import { Form, Field, defineRule } from 'vee-validate';
+
+defineRule("required", value => {
+  if (!value || value.length === 0) {
+    return "Campo obrigatório";
+  }
+  return true;
+});
+
+defineRule("dataNasc", value => {
+  if (new Date(value + ' 00:00:00') <= new Date()) {
+    return true;
+  } else {
+    return "Não pode ser superior a data atual";
+  }
+});
+
+defineRule("nomeCompleto", value => {
+  let regexp = /^[a-zA-Z]{2,}( [a-zA-Z]+)*?( [a-zA-Z]{2,}){1,}$/
+
+  if (!regexp.test(value)) {
+    return "Favor informar o nome completo";
+  }
+  return true;
+});
+
+export default {
+  components: {
+    'vee-form': Form,
+    'vee-field': Field
+  },
+  data() {
+    return {
+      schema: {
+        nome: "required|nomeCompleto",
+        dataNascimento: "required|dataNasc",
+      },
+      pessoa: {}
+    }
+  },
+  methods: {
+    salvarPessoa() {
+      this.$store.commit('inserir', this.pessoa);
+      this.pessoa = {};
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ M1S10/exercicio-07/src/views/pessoas/listagem/Listagem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+      <h3>Listagem</h3>
+      <br><br>
+      <div>
+        <button type="button" class="btn btn-outline-primary btn-sm" @click="buscarPessoas">Listar Pessoas</button>
+      </div>
+      <hr>
+      <div class="zerado" v-if="listaPessoas.length === 0">
+        Não há pessoas cadastradas
+      </div>
+
+      <table class="table" v-else>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Data Nascimento</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="p in listaPessoas" :key="p.id">
+            <td v-text="p.nome"></td>
+            <td v-text="p.dataNascimento"></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listaPessoas: []
+    }
+  },
+  methods: {
+    buscarPessoas() {
+      this.listaPessoas = this.$store.state.pessoas;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
