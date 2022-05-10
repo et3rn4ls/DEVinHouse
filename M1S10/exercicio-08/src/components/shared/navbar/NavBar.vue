7e2aa66b1835bff2d51e400044ba011e85c2f539
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-05-10 19:59:56 -0300

    M1S10 - Exercicio 08

---
+++ M1S10/exercicio-08/src/components/shared/navbar/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <router-link to="/" class="nav-link active" aria-current="page">HOME</router-link>
+                        <router-link to="/pessoas/cadastro" class="nav-link active" aria-current="page">CADASTRO</router-link>
+                        <router-link to="/pessoas/listagem" class="nav-link active" aria-current="page">LISTAGEM</router-link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ M1S10/exercicio-08/src/main.js
@@ -0,0 +1,40 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { createStore } from 'vuex';
+import App from './App.vue';
+import Home from './views/home/Home';
+import Cadastro from './views/pessoas/cadastro/Cadastro';
+import Listagem from './views/pessoas/listagem/Listagem';
+
+const routes = [
+    { path: '/', component: Home, alias: ["/home"] },
+    { path: '/pessoas/cadastro', component: Cadastro },
+    { path: '/pessoas/listagem', component: Listagem }
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHashHistory()
+});
+
+const store = createStore({
+    state() {
+        return {
+            pessoas: []
+        }
+    },
+    mutations: {
+        inserir(state, pessoa) {
+            state.pessoas.push(pessoa);
+        },
+        remover(state, pessoa) {
+            state.pessoas.splice(pessoa, 1);
+        }
+    }
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.mount('#app');
