8b0adb5c120953441e46d6a525a02edc0602c5d6
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-05-14 18:37:50 -0300

    M1S11 - Exercicio 04

---
+++ M1S11/exercicio-04/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+        <NavBar />
+        <hr>
+        <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/shared/navbar/NavBar';
+
+export default {
+    name: 'App',
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ M1S11/exercicio-04/src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h3>Home</h3>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h3 {
+  text-align: center;
+}
+</style>
