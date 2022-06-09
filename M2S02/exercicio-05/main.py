9515e2b725ae305e46e7ae2df544762eb622df09
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-06-09 09:33:40 -0300

    M2S02 - Exercício 05

---
+++ M2S02/exercicio-05/main.py
@@ -0,0 +1,16 @@
+vogais = ['a', 'e', 'i', 'o', 'u']
+
+while True:
+    letra = input('Informe uma letra: ')
+
+    if letra == '0':
+        print ('Saindo...')
+        break
+    elif letra.isdigit() == True:
+        print ('Informe uma letra e não um número!')
+        continue
+    else:
+        if letra in vogais:
+            print('{0} é uma vogal' .format(letra))
+            continue
+        print('{0} é uma consoante' .format(letra))
