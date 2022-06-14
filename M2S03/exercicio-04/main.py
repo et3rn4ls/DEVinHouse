78f765c51742ceb8095235163200a010c5c12abd
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-06-14 11:10:13 -0300

    M2S03 - Exercício 04

--- M2S03/exercicio-04/main.py
+++ M2S03/exercicio-04/main.py
@@ -1,7 +1,7 @@
 def formatar(t, c="-"):
     print(f'{c * 5} {t} {c * 5}')
 
-texto = input('Digite uma palavra: ')
+texto = input('Digite um texto: ')
 caractere = input('Informe o caractere: ')
 
 if len(caractere) == 0:
