5afd0d8059e3ac423c324aa8f97ab3b81d7267ac
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-06-21 21:16:25 -0300

    M2S03 - Exercício 05

---
+++ M2S03/exercicio-05/main.py
@@ -0,0 +1,10 @@
+from random import sample, shuffle  
+
+N = [x for x in sample(range(1, 100), 20)]
+
+shuffle(N)
+print(N)
+numero = min(N)
+posicao = N.index(numero)
+
+print(f'O menor elemento de N é {numero} e a sua posição dentro da lista é {posicao}')
