c2b78985ea805687907510fb9bcdc407951e6241
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-06-21 22:12:33 -0300

    M2S03 - Exercício 08

---
+++ M2S03/exercicio-08/main.py
@@ -0,0 +1,24 @@
+import json
+
+file = open('lista.json', 'r')
+lista = file.readlines()
+file.close()
+
+for i in lista:
+    d = (i.split(';'))
+
+v = ''
+dicio = dict.fromkeys(d, v)
+print(dicio)
+
+dicio.update({'primeiro_nome': 'Adriano'})
+print(dicio)
+
+dicio.update({'segundo_nome': 'Matos'})
+print(dicio)
+
+dicio.update({'idade': 42})
+print(dicio)
+
+with open('arquivo.json', 'w') as convert_file: 
+    convert_file.write(json.dumps(dicio))
