7f7090a80a54f4c79dde01f53a29ab375bac2d0f
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-06-21 21:42:25 -0300

    M2S03 - Exercício 06

---
+++ M2S03/exercicio-06/main.py
@@ -0,0 +1,11 @@
+pessoa = {'primeiro_nome': '', 'segundo_nome': '', 'idade': ''}
+print(pessoa)
+
+pessoa.update({'primeiro_nome': 'Adriano'})
+print(pessoa)
+
+pessoa.update({'segundo_nome': 'Matos'})
+print(pessoa)
+
+pessoa.update({'idade': 42})
+print(pessoa)
