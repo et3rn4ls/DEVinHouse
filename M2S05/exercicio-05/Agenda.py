85fcf8f642d9632f4bfb67771dacf2229d5b4a27
Adriano Matos Meier <adriano_meier@estudante.sc.senai.br>
2022-07-01 21:31:41 -0300

    M2S05 - Exercício 05

--- M2S05/exercicio-05/Agenda.py
+++ M2S05/exercicio-05/Agenda.py
@@ -1,8 +1,16 @@
+import json
+import os
+from os.path import exists
+
 from Medico import Medico
 from Paciente import Paciente
 
 
-class Agenda:
+filedb = os.path.join('./', 'data', 'agendadb.json')
+agendas = []
+
+
+class Agenda(Paciente, Medico):
 
     def __init__(self, crm_medico: int, cpf_paciente: str, dia: int, mes: int, ano: int, hora: str, observacao: str):
         self.crm_medico = crm_medico
@@ -15,12 +23,63 @@ class Agenda:
     
     @staticmethod
     def cadastrar_agenda():
-        pass
+        crm_medico = int(input('CRM Medico: '))
+        cpf_paciente = input('CPF Paciente: ')
+        dia = int(input('Dia: '))
+        mes = int(input('Mes: '))
+        ano = int(input('Ano: '))
+        hora = input('Hora: ')
+        observacao = input('Observacao: ')
+
+        a = Agenda(crm_medico, cpf_paciente, dia, mes, ano, hora, observacao)
+        a.__salvar_agenda()
 
     @staticmethod
     def exibir_agenda():
-        pass
+        with open(filedb) as agendadb:
+            print(json.load(agendadb))
 
     def __salvar_agenda(self):
-        pass
+        print('\nSalvando...')
+        keys = ["crm_medico", "cpf_paciente", "dia", "mes", "ano", "hora", "observacao"]
+        values = [self.crm_medico, self.cpf_paciente, self.dia, self.mes, self.ano, self.ano, self.observacao]
+        agenda = dict(zip(keys, values))
+        agendas.append(agenda)
+
+        if exists(filedb):
+            with open(filedb) as agendadb:
+                listaAgendas = json.load(agendadb)
+                listaAgendas.append(agenda)
+            with open(filedb, "w") as agendadbrw:
+                json.dump(listaAgendas, agendadbrw)
+        else:
+            with open(filedb, "w") as agendadbrw:
+                json.dump(agendas, agendadbrw)
+
+        print('\nAgenda salva!')
+
+
+if __name__ == "__main__":
+
+    while True:
+        print("""
+            Selecione uma opção:
+
+            [1] Cadastrar agenda
+            [2] Exibir agendas
+            [0] Sair
+        """)
+
+        op = int(input('Opção: '))
+
+        if op == 0:
+            print ('Saindo ...')
+            break
+
+        if op == 1:
+            Agenda.cadastrar_agenda()
+        elif op == 2:
+            Agenda.exibir_agenda()
+        else:
+            print('Opção inválida!')
 
